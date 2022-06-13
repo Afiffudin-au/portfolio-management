bcc1affcb5184ec25d61e021a1d5518c26faa11a
Afiffudin-au <afiffudin077@gmail.com>
2022-06-13 13:48:01 +0700

    edit User (basic-auth)

--- src/components/User/UserContent.tsx
+++ src/components/User/UserContent.tsx
@@ -4,6 +4,9 @@ import UserForm from './UserForm'
 function UserContent() {
   return (
     <div className='py-2 w-9/12 mx-auto'>
+      <h2 className='font-bold text-gray-800 text-lg '>
+        for this demo version. Use username : demo and password : demo.
+      </h2>
       <p className='text-md text-gray-800 font-medium'>
         For the first time use http basic authentication to use portfolio
         management, http basic authentication is used as key for API calls. If
