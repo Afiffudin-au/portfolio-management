a89c5a98b9c722ca4c690eb7a96e671a9b2bac51
Afiffudin-au <afiffudin077@gmail.com>
2022-06-13 10:21:20 +0700

    remove the basic-auth feature that uses .env

--- src/api-config/index.js
+++ src/api-config/index.js
@@ -1,25 +1,4 @@
 export const api_config = {
-  //For basic auth using .env file
-  USERNAME: process.env.REACT_APP_USERNAME,
-  PASSWORD: process.env.REACT_APP_PASSWORD,
-
-  //Important!
   BASE_URL: process.env.REACT_APP_BASE_URL,
   API_VERSION: process.env.REACT_APP_API_VERSION,
 }
-
-//For basic auth using .env file
-export const headers_auth = {
-  Authorization: `Basic ${btoa(
-    `${api_config.USERNAME}:${api_config.PASSWORD}`
-  )}`,
-}
-
-// Example : Set up basic authentication with axios at api-calls/callAPI.ts (with .env)
-/*const res = await axios({
-  method: method,
-  url: url,
-  data :data,
-  headers : headers_auth
-})
-*/
