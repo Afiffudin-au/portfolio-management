14dfe8f435a3e8ba2ceb964c76dc73b2a4c1af60
Afiffudin-au <afiffudin077@gmail.com>
2022-06-15 21:23:27 +0700

    fix table layout

--- src/components/Project/TableItem.tsx
+++ src/components/Project/TableItem.tsx
@@ -93,19 +93,21 @@ function TableItem({
             ))}
           </div>
         </td>
-        <td className='px-2 py-2 font-medium text-sm text-gray-900 flex items-center justify-start gap-2'>
-          <button
-            onClick={() => setIsOpenModal(true)}
-            type='button'
-            className='font-medium text-sm text-white bg-blue-500 py-2 px-5 rounded-sm shadow-sm'>
-            Edit
-          </button>
-          <button
-            onClick={handleDelete}
-            type='button'
-            className='font-medium text-sm text-white bg-red-500 py-2 px-5 rounded-sm shadow-sm'>
-            Delete
-          </button>
+        <td className='px-2 py-2 font-medium text-sm text-gray-900'>
+          <div className='flex gap-2'>
+            <button
+              onClick={() => setIsOpenModal(true)}
+              type='button'
+              className='font-medium text-sm text-white bg-blue-500 py-2 px-5 rounded-sm shadow-sm'>
+              Edit
+            </button>
+            <button
+              onClick={handleDelete}
+              type='button'
+              className='font-medium text-sm text-white bg-red-500 py-2 px-5 rounded-sm shadow-sm'>
+              Delete
+            </button>
+          </div>
         </td>
       </tr>
       {isOpenModal && (
